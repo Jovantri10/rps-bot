a29016e8116cc38bbe7e757587a1202500f5aad8
kwugfighter <isaacli430@gmail.com>
2017-11-22 22:35:57 +0800

    a

--- bot.py
+++ bot.py
@@ -82,12 +82,12 @@ class RPSBot(commands.Bot):
     @commands.command(name='help')
     async def _help(self, ctx, command=None):
         '''Shows this page'''
+        em = discord.Embed(title='Help', color=0x181818)
         for cog in self.cogs:
             em.add_field(name=str(cog), value="```\n"+'\n\n'.join([f"{ctx.prefix}{attr.name}{' '*(10-len(attr.name))}{attr.short_doc}" for name, attr in inspect.getmembers(cog) if isinstance(attr, commands.Command)])+'\n```')
         if command:
             command = discord.utils.get(self.commands, name=command.lower())
             return await ctx.send(embed=discord.Embed(color=0x181818, title=f"``{ctx.prefix}{command.signature}``", description=command.short_doc))
-        em = discord.Embed(title='Help', color=0x181818)
         em.set_author(name='Royale Prestige Series', icon_url=self.user.avatar_url)
         commands = []
         for command in self.commands:
