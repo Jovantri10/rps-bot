62392c0bcbcc5f12729859250b52e524de5e5796
kwugfighter <isaacli430@gmail.com>
2018-04-28 12:35:12 +0800

    a

--- bot.py
+++ bot.py
@@ -45,9 +45,8 @@ class RPSBot(commands.Bot):
         print(f"Bot is ready! Invite: {discord.utils.oauth_url(self.user.id, perms)}")
 
     async def on_raw_reaction_add(self, payload):
-        print(payload.emoji.id)
-        # if reaction.message.id == self.role_message_ids[0] and reaction.emoji.id == 429157195117232128:
-        #     await user.add_roles(discord.utils.get(user.guild.roles, id=393217384112193557))
+        if payload.message_id == self.role_message_ids[0] and payload.emoji.id == 429157195117232128:
+            await user.add_roles(discord.utils.get(user.guild.roles, id=393217384112193557))
             
 
     async def on_member_join(self, member):
