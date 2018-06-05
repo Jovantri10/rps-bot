473ae8ccd63eb54e9c6f18a1940323bb55f6ccc5
kwugfighter <isaacli430@gmail.com>
2018-06-05 20:26:14 +0800

    a

--- cogs.py
+++ cogs.py
@@ -347,7 +347,6 @@ class Cog:
             if video.startswith("http://youtube.com/watch") or video.startswith("https://youtube.com/watch") or video.startswith("https://www.youtube.com/watch") or video.startswith("http://www.youtube.com/watch"):
                 url = video
                 name = await self.get_name_from_vid(video)
-                print(name)
                 if not name:
                     return await ctx.send("That's not a valid url!")
             else:
